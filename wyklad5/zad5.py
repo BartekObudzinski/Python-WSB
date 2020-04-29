1a330b48904678cac1dad608a30e8ac7951bf5ad
Bartłomiej Obudziński <obudzinskib@gmail.com>
2020-04-29 14:00:00 +0200

    Update zad5.py

--- wyklad5/zad5.py
+++ wyklad5/zad5.py
@@ -2,7 +2,7 @@ a = int(input("Podaj liczbe a "))
 b = int(input("Podaj liczbe b "))
 c = int(input("Podaj liczbe c "))
 
-if(a>0 and a <=10 and a>b and b>c ):
+if(a>0 and a <=10 and a>b or b>c ):
     print("warunek spelniony")
 else:
     print("warunek nie spelniony")
