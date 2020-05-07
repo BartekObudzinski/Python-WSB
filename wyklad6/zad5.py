5570e64f207243b3efe8e325b5a4cfb7bcfdacf8
Bartłomiej Obudziński <obudzinskib@gmail.com>
2020-05-07 14:29:18 +0200

    Create zad5.py

---
+++ wyklad6/zad5.py
@@ -0,0 +1,11 @@
+def dwieProste(a1,a2):
+    if(a1==a2):
+        print("Proste sa rownolegle")
+    elif(a1*a2==-1):
+        print("Proste sa prostopadle")
+    else:
+        print("Proste nie sa ani rownolegle ani prostopadle")
+
+dwieProste(2,2)
+dwieProste(-1,1)
+dwieProste(2,3)
