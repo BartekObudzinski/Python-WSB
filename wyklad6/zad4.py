f29185f27b170aa733ef48c209d7b9bc3f398c32
Bartłomiej Obudziński <obudzinskib@gmail.com>
2020-05-07 13:57:47 +0200

    Create zad4.py

---
+++ wyklad6/zad4.py
@@ -0,0 +1,11 @@
+def monotonicznosc(a):
+    if(a>0):
+        print("Funkcja rosnąca")
+    elif(a==0):
+        print("Funkcja stała")
+    else:
+        print("Funkcja malejąca")
+
+monotonicznosc(5)
+monotonicznosc(0)
+monotonicznosc(-12)
